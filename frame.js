2e896251da535ff8a13481215cd07d83ff146918
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-27 21:54:32 -0400

    Improves frame handler

--- frame.js
+++ frame.js
@@ -1,9 +1,12 @@
-let lastFpsDisplayTick
-let lastFrameCount
 const callbacks = {
   fps: [],
   paint: []
 }
+
+let lastFpsDisplayTick
+let lastFrameCount
+let lastFps = 0
+let stability = 0
 
 function paint () {
   const tick = performance.now()
@@ -11,9 +14,15 @@ function paint () {
   const ticks = tick - lastFpsDisplayTick
   if (ticks > 250) {
     const fps = Math.floor(1000 * lastFrameCount / ticks)
+    if (fps === lastFps) {
+      ++stability
+    } else {
+      lastFps = fps
+      stability = 0
+    }
     lastFpsDisplayTick = tick
     lastFrameCount = 0
-    callbacks.fps.forEach(callback => callback(fps))
+    callbacks.fps.forEach(callback => callback(fps, stability))
   }
   callbacks.paint.forEach(callback => callback(tick))
   window.requestAnimationFrame(paint)
