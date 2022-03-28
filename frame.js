7daf3591013096e0e50b3c4ec7ecd1512cdd57ef
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-27 21:34:45 -0400

    Change interface

--- frame.js
+++ frame.js
@@ -1,35 +1,37 @@
-let title
 let lastFpsDisplayTick
 let lastFrameCount
-const callbacks = []
+const callbacks = {
+  fps: [],
+  paint: []
+}
 
 function paint () {
   const tick = performance.now()
   ++lastFrameCount
   const ticks = tick - lastFpsDisplayTick
-  if (ticks > 500) {
+  if (ticks > 250) {
     const fps = Math.floor(1000 * lastFrameCount / ticks)
-    document.title = title + ' [' + fps + ']'
     lastFpsDisplayTick = tick
     lastFrameCount = 0
+    callbacks.fps.forEach(callback => callback(fps))
   }
-  callbacks.forEach(callback => callback(tick))
+  callbacks.paint.forEach(callback => callback(tick))
   window.requestAnimationFrame(paint)
 }
 
-export function register (callback) {
-  callbacks.push(callback)
+export function register (type, callback) {
+  callbacks[type].push(callback)
 }
 
-export function unregister (callback) {
-  const pos = callbacks.indexOf(callback)
+export function unregister (type, callback) {
+  const list = callbacks[type]
+  const pos = list.indexOf(callback)
   if (pos !== -1) {
-    callbacks.splice(pos, 1)
+    list.splice(pos, 1)
   }
 }
 
 export function start () {
-  title = document.title
   lastFpsDisplayTick = performance.now()
   lastFrameCount = 0
   window.requestAnimationFrame(paint)
